56f43620e4054c8ee743d74c9dbe43442739a777
ipcode-cloud <ipcode0@gmail.com>
2025-05-19 14:23:49 +0200

    Refactor server setup: add database connection and JSON middleware; update .gitignore to include .env

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
-import express from 'express'
+import express from "express";
+import { connectDB } from "./config/db";
 
-const app= express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+connectDB();
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
